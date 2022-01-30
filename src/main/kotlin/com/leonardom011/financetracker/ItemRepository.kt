81ae38468a60e0a3c5329da3f3e5fbaae2fa5b35
Leonardo Marinovic <leonardo.leo.201@gmail.com>
2022-01-30 22:44:12 +0100

    Added rest and improved database

--- src/main/kotlin/com/leonardom011/financetracker/ItemRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.leonardom011.financetracker
-
-import org.springframework.data.repository.CrudRepository
-
-interface ItemRepository : CrudRepository<Item, Long> {
-    fun findBySlug(slug: String): Item?
-}
